638ab0861baf94dcb0373f9f65c386de65af4bee
Mehul <65443164+infiniteregrets@users.noreply.github.com>
2023-02-11 08:23:57 -0500

    Add support to fetch twitter data (#1)

--- main.py
+++ main.py
@@ -1,7 +1,95 @@
+import datetime
+import httpx
+import json
+import july
+from july.utils import date_range
 import matplotlib.pyplot as plt
 import calendar
 
+headers = {
+    "authority": "api.twitter.com",
+    "accept": "*/*",
+    "authorization": "Bearer TOKEN",
+    "x-guest-token": "TOKEN",
+    "x-csrf-token": "TOKEN",
+}
+
+params = {
+    "include_profile_interstitial_type": "1",
+    "include_blocking": "1",
+    "include_blocked_by": "1",
+    "include_followed_by": "1",
+    "include_want_retweets": "1",
+    "include_mute_edge": "1",
+    "include_can_dm": "1",
+    "include_can_media_tag": "1",
+    "include_ext_has_nft_avatar": "1",
+    "include_ext_is_blue_verified": "1",
+    "include_ext_verified_type": "1",
+    "skip_status": "1",
+    "cards_platform": "Web-12",
+    "include_cards": "1",
+    "include_ext_alt_text": "true",
+    "include_ext_limited_action_results": "false",
+    "include_quote_count": "true",
+    "include_reply_count": "1",
+    "tweet_mode": "extended",
+    "include_ext_collab_control": "true",
+    "include_ext_views": "true",
+    "include_entities": "true",
+    "include_user_entities": "true",
+    "include_ext_media_color": "true",
+    "include_ext_media_availability": "true",
+    "include_ext_sensitive_media_warning": "true",
+    "include_ext_trusted_friends_metadata": "true",
+    "send_error_codes": "true",
+    "simple_quoted_tweet": "true",
+    "q": '"caramel brulee" until:2022-01-01 since:2022-01-02',
+    "tweet_search_mode": "live",
+    "query_source": "typed_query",
+    "count": "20",
+    "requestContext": "launch",
+    "pc": "1",
+    "spelling_corrections": "1",
+    "include_ext_edit_control": "true",
+    "ext": "mediaStats,highlightedLabel,hasNftAvatar,voiceInfo,enrichments,superFollowMetadata,unmentionInfo,editControl,collab_control,vibe",
+}
+
+url = "https://api.twitter.com/2/search/adaptive.json"
+
+date_generator = (
+    datetime.datetime(2022, 1, 1) + datetime.timedelta(days=i) for i in range(368)
+)
+
 data = []
+
+from_date, until_date = next(date_generator).strftime("%Y-%m-%d"), next(
+    date_generator
+).strftime("%Y-%m-%d")
+
+for i in range(366):
+    params["q"] = f'"caramel brulee" until:{until_date} since:{from_date}'
+    response = httpx.get(url, headers=headers, params=params)
+    response_dict = json.loads(response.text)
+    data.append(len(response_dict["globalObjects"]["tweets"].keys()))
+    from_date, until_date = until_date, next(date_generator).strftime("%Y-%m-%d")
+
+
+print(data)
+
+
+dates = date_range("2022-01-01", "2022-12-31")
+
+"""
+Heatmap using july (horizontal, github style) -
+"""
+
+heatmap = july.heatmap(dates, data, title="Caramel Brulée!", cmap="github")
+plt.savefig("heatmap2.png")
+
+"""
+Heatmap using matplotlib directly (vertical) -
+"""
 
 max_data, min_data = max(data), min(data)
 
